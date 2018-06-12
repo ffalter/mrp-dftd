f1f4de6203ca7d9ded3a0f19f4abf27fff5337d8
ffalter <falter.florian@t-online.de>
2018-06-12 12:35:09 +0200

    Environment refactoring

--- Implementation/Simulation/src/controls/FirstSimpleBuilder.java
+++ Implementation/Simulation/src/controls/FirstSimpleBuilder.java
@@ -7,8 +7,6 @@ import repast.simphony.context.space.graph.NetworkBuilder;
 import repast.simphony.context.space.grid.GridFactory;
 import repast.simphony.context.space.grid.GridFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
-import repast.simphony.engine.environment.RunEnvironment;
-import repast.simphony.parameter.Parameters;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.NdPoint;
 import repast.simphony.space.continuous.RandomCartesianAdder;
@@ -80,15 +78,8 @@ public class FirstSimpleBuilder implements ContextBuilder<Object> {
 	}
 	
 	public void setParams() {
-		final Parameters params = RunEnvironment.getInstance().getParameters();
-		Environment.getInstance().infectionRate = params.getDouble("infectionRate");
-		Environment.getInstance().infectionRateMating = params.getDouble("infectionRateMating");
-		Environment.getInstance().femaleRatio = params.getDouble("femaleRatio");
-		Environment.getInstance().interactionRadius= params.getInteger("interactionRadius");
-		Environment.getInstance().mapSizeX= params.getInteger("mapSizeX");
-		Environment.getInstance().mapSizeY= params.getInteger("mapSizeY");
-		Environment.getInstance().populationSize= params.getInteger("populationSize");
-		Environment.getInstance().steprange= params.getInteger("stepRange");
+		// trigger first initialization
+		Environment.getInstance();
 	}
 
 	
--- Implementation/Simulation/src/simulation/Environment.java
+++ Implementation/Simulation/src/simulation/Environment.java
@@ -1,30 +1,82 @@
 package simulation;
 
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.parameter.Parameters;
+
 public class Environment {
 
 	// singleton
-	private static Environment instance = new Environment();
+	private static Environment instance;
 	
-	private Environment() {}
-
-	public static Environment getInstance() {
-		if (Environment.instance == null) {
-			Environment.instance = new Environment ();
-		    }
-		    return Environment.instance;
+	private Environment() 
+	{
+		final Parameters params = RunEnvironment.getInstance().getParameters();
+		this.infectionRate = params.getDouble("infectionRate");
+		this.infectionRateMating = params.getDouble("infectionRateMating");
+		this.femaleRatio = params.getDouble("femaleRatio");
+		this.interactionRadius = params.getInteger("interactionRadius");
+		this.mapSizeX = params.getInteger("mapSizeX");
+		this.mapSizeY = params.getInteger("mapSizeY");
+		this.populationSize = params.getInteger("populationSize");
+		this.steprange = params.getInteger("stepRange");
+		this.ticksPerYear = params.getInteger("tickValue");
 	}
 
-	public int mapSizeX;
-	public int mapSizeY;
-	public int populationSize;
-	public double femaleRatio;
+	public static Environment getInstance() {
+		if (instance == null) {
+			instance = new Environment ();
+		    }
+		    return instance;
+	}
 
-	public int steprange;
-	public int interactionRadius;
+	private int mapSizeX;
+	private int mapSizeY;
+	private int populationSize;
+	private int ticksPerYear;
+	private double femaleRatio;
+
+	private int steprange;
+	private int interactionRadius;
 
 	// probability of getting infected during interaction
-	public double infectionRate;
+	private double infectionRate;
 	// probability of getting infected male - female (only during mating time)
-	public double infectionRateMating;
+	private double infectionRateMating;
+
+	public int getMapSizeX() {
+		return mapSizeX;
+	}
+
+	public int getMapSizeY() {
+		return mapSizeY;
+	}
+
+	public int getPopulationSize() {
+		return populationSize;
+	}
+
+	public int getTicksPerYear() {
+		return ticksPerYear;
+	}
+
+	public double getFemaleRatio() {
+		return femaleRatio;
+	}
+
+	public int getSteprange() {
+		return steprange;
+	}
+
+	public int getInteractionRadius() {
+		return interactionRadius;
+	}
+
+	public double getInfectionRate() {
+		return infectionRate;
+	}
+
+	public double getInfectionRateMating() {
+		return infectionRateMating;
+	}
 
 }

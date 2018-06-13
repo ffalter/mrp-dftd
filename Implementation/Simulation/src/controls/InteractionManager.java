d1a3db43dd6547a173b77b0cdb42bf07c8cf2e2d
ffalter <falter.florian@t-online.de>
2018-06-13 11:18:43 +0200

    InterActionManager fix

--- Implementation/Simulation/src/controls/InteractionManager.java
+++ Implementation/Simulation/src/controls/InteractionManager.java
@@ -22,13 +22,13 @@ public class InteractionManager {
 		epsilon = Environment.getInstance().getVaccinatedInfectionRate();
 		delta = Environment.getInstance().getInfectionMatingFactor();
 		x = Environment.getInstance().getInfectionRate();
-		interactionMatrix = new double [dft.DFT2.ordinal()][gender.MALE.ordinal()][states.SICK.ordinal()][gender.MALE.ordinal()][states.SICK.ordinal()];
+		interactionMatrix = new double [dft.DFT2.ordinal()+1][gender.MALE.ordinal()+1][states.SICK.ordinal()+1][gender.MALE.ordinal()+1][states.SICK.ordinal()+1];
 		
-		for (int df=0;df<dft.DFT2.ordinal();df++) {
-			for(int gen=0;gen<gender.MALE.ordinal();gen++) {
-				for(int state=0;state<states.SICK.ordinal();state++) {
-					for(int gen2=0;gen2<gender.MALE.ordinal();gen2++) {
-						for(int state2=0;state2<states.SICK.ordinal();state2++) {
+		for (int df=0;df<dft.DFT2.ordinal()+1;df++) {
+			for(int gen=0;gen<gender.MALE.ordinal()+1;gen++) {
+				for(int state=0;state<states.SICK.ordinal()+1;state++) {
+					for(int gen2=0;gen2<gender.MALE.ordinal()+1;gen2++) {
+						for(int state2=0;state2<states.SICK.ordinal()+1;state2++) {
 							interactionMatrix[df][gen][states.SICK.ordinal()][gen2][state2] = 1;
 							interactionMatrix[df][gen][states.VACCINATED.ordinal()][gen2][states.SICK.ordinal()] = epsilon;
 							interactionMatrix[df][gen][states.HEALTHY.ordinal()][gen2][states.SICK.ordinal()] = x;

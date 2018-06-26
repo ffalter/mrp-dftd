0b1421b3bfe0e6b087a209a472c8c54b3ff454f2
Timo Raff <timoraff@gmail.com>
2018-06-26 12:52:04 +0200

    added terminaion if no sick devils exist anymore (out commented)

--- Implementation/Simulation/src/controls/TerminationControl.java
+++ Implementation/Simulation/src/controls/TerminationControl.java
@@ -4,6 +4,7 @@ import repast.simphony.context.Context;
 import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.engine.schedule.ScheduleParameters;
 import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.util.collections.IndexedIterable;
 import simulation.Environment;
 import simulation.TasmanianDevil;
 
@@ -15,10 +16,21 @@ public class TerminationControl {
 	}
 	@ScheduledMethod(start = 1, interval = 1, priority = ScheduleParameters.FIRST_PRIORITY)
 	public void testForEnd() {
-		int totalPopulation = context.getObjects(TasmanianDevil.class).size();
+		IndexedIterable<Object> totalPopulation = context.getObjects(TasmanianDevil.class);
 		// Stop the simulator
-		if (totalPopulation < Environment.getInstance().getMinPopulationSize()) {
+		if (totalPopulation.size() < Environment.getInstance().getMinPopulationSize()) {
 			RunEnvironment.getInstance().endRun();
 		}
+		
+		/*
+		for(Object obj: totalPopulation) {
+			TasmanianDevil td= (TasmanianDevil)obj;
+			if(td.getSickDFT1()>0||td.getSickDFT2()>0) {
+				return;
+			}
+		}
+		RunEnvironment.getInstance().endRun();
+		*/
+		
 	}
 }
